115eeede8d533d26aabd06fdacd249d7905694d1
linbinghe <linbinghe@gmail.com>
2018-05-23 22:57:32 +0800

    完善测试FBO

--- app/src/main/java/com/lbh/lesson/gles/MainActivity.java
+++ app/src/main/java/com/lbh/lesson/gles/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lbh.lesson.gles;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,11 +19,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initView();
+
+        checkPermission();
     }
 
     private void initView() {
         findViewById(R.id.b_triangle).setOnClickListener(this);
         findViewById(R.id.b_texture).setOnClickListener(this);
+    }
+
+    private boolean checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_PHONE_STATE}, 100);
+            return false;
+        }
+        return true;
     }
 
     @Override
--- app/src/main/java/com/lbh/lesson/gles/lesson2/TextureRender.java
+++ app/src/main/java/com/lbh/lesson/gles/lesson2/TextureRender.java
@@ -5,12 +5,18 @@ import android.graphics.Bitmap;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
+import android.util.Log;
 
 import com.lbh.lesson.gles.util.CoordinateUtil;
 import com.lbh.lesson.gles.util.ImageUtil;
 import com.lbh.lesson.gles.util.OpenGLUtil;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -63,12 +69,16 @@ public class TextureRender implements GLSurfaceView.Renderer {
     private int mTexture; //纹理句柄
     private int mInputTextureCoordinate; //纹理坐标句柄
 
+    private int[] mFrameBuffers; //FBO句柄
+    private int[] mFrameBufferTextures; //FBO对应纹理句柄
+
     private int[] mTextureId = new int[]{OpenGLUtil.NO_TEXTURE}; //用于绘制的纹理ID
 
     private FloatBuffer mVertexBuffer; //顶点坐标数据
     private FloatBuffer mTextureBuffer; //纹理坐标数据
 
     private Context mContext;
+    private int mVPWidth, mVPHeight;
 
     public TextureRender(Context mContext) {
         this.mContext = mContext;
@@ -101,11 +111,51 @@ public class TextureRender implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        mVPWidth = width;
+        mVPHeight = height;
+
+        processViewPortChanged();
+    }
+
+    protected void processViewPortChanged() {
+        mFrameBuffers = new int[1];
+        mFrameBufferTextures = new int[1];
+        GLES20.glGenTextures(1, mFrameBufferTextures, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFrameBufferTextures[0]);
+        GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, mVPWidth,
+                mVPHeight, 0,
+                GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
+
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+
+
+        GLES20.glGenFramebuffers(1, mFrameBuffers, 0);
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, mFrameBuffers[0]);
+        GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER,
+                GLES20.GL_COLOR_ATTACHMENT0,
+                GLES20.GL_TEXTURE_2D, mFrameBufferTextures[0], 0);
+        int status = GLES20.glCheckFramebufferStatus(GLES20.GL_FRAMEBUFFER);
+        if (status != GLES20.GL_FRAMEBUFFER_COMPLETE) {
+            Log.e("lbh", "status != GL_FRAMEBUFFER_COMPLETE: " + status);
+        }
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         //这里网上很多博客说是设置背景色，其实更严格来说是通过所设置的颜色来清空颜色缓冲区，改变背景色只是其作用之一
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, mFrameBuffers[0]);
+        GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0,
+                GLES20.GL_TEXTURE_2D, mFrameBufferTextures[0], 0);
         GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);//白色不透明
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
@@ -141,6 +191,107 @@ public class TextureRender implements GLSurfaceView.Renderer {
         GLES20.glDisableVertexAttribArray(mInputTextureCoordinate);
         //解绑纹理
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+
+//        getBitmap();
+
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+
+
+        //告知OpenGL所要使用的Program
+        GLES20.glUseProgram(mProgramId);
+
+        //启用指向渲染图形所处的顶点数据的句柄
+        GLES20.glEnableVertexAttribArray(mPositionId);
+        //绑定渲染图形所处的顶点数据
+        GLES20.glVertexAttribPointer(mPositionId, CoordinateUtil.PER_COORDINATE_SIZE,
+                GLES20.GL_FLOAT, false,
+                CoordinateUtil.COORDINATE_STRIDE, mVertexBuffer);
+
+        //启用指向纹理坐标数据的句柄
+        GLES20.glEnableVertexAttribArray(mInputTextureCoordinate);
+        //绑定纹理坐标数据
+        GLES20.glVertexAttribPointer(mInputTextureCoordinate, CoordinateUtil.PER_COORDINATE_SIZE,
+                GLES20.GL_FLOAT, false, CoordinateUtil.COORDINATE_STRIDE, mTextureBuffer);
+
+        //将纹理对象设置到0号单元上
+        GLES20.glUniform1i(mTexture, 0);
+        //激活GL_TEXTURE0，该纹理单元默认激活
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        //绑定纹理数据到GL_TEXTURE0纹理单元上
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFrameBufferTextures[0]);
+
+        //使用GL_TRIANGLE_FAN方式绘制纹理
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, CoordinateUtil.VERTEX_COUNT);
+
+        //禁用顶点数据对象
+        GLES20.glDisableVertexAttribArray(mPositionId);
+        //禁用纹理坐标数据对象
+        GLES20.glDisableVertexAttribArray(mInputTextureCoordinate);
+        //解绑纹理
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+
+//        getBitmap();
+    }
+
+    public void getBitmap() {
+        final int[] pixelMirroredArray = new int[mVPWidth * mVPHeight];
+
+        IntBuffer pixelBuffer = IntBuffer.allocate(mVPWidth * mVPHeight);
+        GLES20.glReadPixels(0, 0, mVPWidth, mVPHeight, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE,
+                pixelBuffer);
+        int[] pixelArray = pixelBuffer.array();
+        for (int i = 0; i < mVPHeight; i++) {
+            for (int j = 0; j < mVPWidth; j++) {
+                pixelMirroredArray[(mVPHeight - i - 1) * mVPWidth + j] =
+                        pixelArray[i * mVPWidth + j];
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(mVPWidth, mVPHeight, Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(IntBuffer.wrap(pixelMirroredArray));
+
+        try {
+            bitmap2File(bitmap, "/sdcard/PhotonLib/cache/" + System.currentTimeMillis() + ".png", 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkAndMakeDirs(String path) {
+        File dstFile = new File(path);
+        if (dstFile != null) {
+            if (dstFile.isDirectory()) {
+                if (!dstFile.exists()) {
+                    dstFile.mkdirs();
+                }
+            } else {
+                if (dstFile.getParentFile() != null && !dstFile.getParentFile().exists()) {
+                    dstFile.getParentFile().mkdirs();
+                }
+            }
+        }
+    }
+
+    public static void bitmap2File(Bitmap bitmap, String path, int quality) throws IOException {
+        File file = new File(path);
+        if (file.exists()) {
+            file.delete();
+        }
+        checkAndMakeDirs(path);
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(file);
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, quality, out)) {
+                out.flush();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void loadTexture() {
@@ -181,7 +332,7 @@ public class TextureRender implements GLSurfaceView.Renderer {
 
     public void destroy() {
         if (mTextureId[0] != OpenGLUtil.NO_TEXTURE) {
-            GLES20.glDeleteTextures(1, mTextureId,0);
+            GLES20.glDeleteTextures(1, mTextureId, 0);
         }
         GLES20.glDeleteProgram(mProgramId);
     }
